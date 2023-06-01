6177e9b28edc07b11dfe8e1110b4d9b129b35abd
Matheus Ramaldes | Front - End Developer <65913756+Ramald@users.noreply.github.com>
2023-06-01 11:36:33 -0300

    Add files via upload

---
+++ Pizzaria/app.js
@@ -0,0 +1,11 @@
+
+function clicker() {
+    let m = document.getElementById("menu");
+    if(m.style.display == 'none') {
+      m.style.display = 'block';
+    } else {
+      m.style.display = 'none';
+    }
+
+    
+  }
